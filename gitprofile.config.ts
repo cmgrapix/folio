594475a0c9bfbcea74cff1f14e6c1834b21783fe
C. <140546088+cmgrapix@users.noreply.github.com>
2025-04-02 21:26:49 -0700

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -13,13 +13,13 @@ const CONFIG = {
   projects: {
     github: {
       display: true, // Display GitHub projects?
-      header: 'Github Projects',
+      header: 'Projects',
       mode: 'automatic', // Mode can be: 'automatic' or 'manual'
       automatic: {
         sortBy: 'stars', // Sort projects by 'stars' or 'updated'
         limit: 8, // How many projects to display.
         exclude: {
-          forks: false, // Forked projects will not be displayed if set to true.
+          forks: true, // Forked projects will not be displayed if set to true.
           projects: [], // These projects will not be displayed. example: ['arifszn/my-project1', 'arifszn/my-project2']
         },
       },
@@ -55,7 +55,7 @@ const CONFIG = {
     telegram: '',
     website: 'https://cmgrapix.github.io/folio/',
     phone: '',
-    email: '',
+    email: 'cmgrapix7@outlook.com',
   },
   resume: {
     fileUrl:
